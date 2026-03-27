81cfa814daade07ed853967f1d48e59836108247
Nam <namtran1066@gmail.com>
2026-03-27 13:45:18 -0400

    fixed eval

--- LLM/Transformer_Implementation/eval.py
+++ LLM/Transformer_Implementation/eval.py
@@ -34,33 +34,39 @@ def translate(sentence, config):
 
     model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size())
 
-    #Retrieve latest model
-    if (config['preload']):
-        model_filename = get_weights_file_path(config, config['preload'])
-        state = torch.load(model_filename)
 
-    #Encoder and decoder input/output
-    encoder_input = get_encoder_input(text, tokenizer_src, tokenizer_tgt, text.size(-1))
-    encoder_output = model.encode(encoder_input.unsqueeze(0), None) #No masking, (1, seq)
-    decoder_input = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
+    model.eval()
+    with torch.no_grad():
+        #Retrieve latest model
+        if (config['preload']):
+            model_filename = get_weights_file_path(config, config['preload'])
+            state = torch.load(model_filename)
+            model.load_state_dict(state['model_state_dict'])
+
+            #Encoder and decoder input/output
+            encoder_input = get_encoder_input(text, tokenizer_src, tokenizer_tgt, text.size(-1))
+            encoder_output = model.encode(encoder_input.unsqueeze(0), None) #No masking, (1, seq)
+            decoder_input = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
     
     
 
-    #Inference loop
-    while (tokenizer_tgt.token_to_id("[EOS]") not in decoder_input):
-        if len(decoder_input) > 1:
+        #Inference loop
+
+        while True:
             decoder_mask = get_decoder_mask(len(decoder_input))
-        else:
-            decoder_mask = None
 
-        decoder_output = model.decode(decoder_input.unsqueeze(0), encoder_output, None, decoder_mask)
-        Proj_output = model.projectionLayer(decoder_output) #(1,seq,vocab)
-        output = torch.softmax(Proj_output, dim = -1).squeeze()
+            decoder_output = model.decode(decoder_input.unsqueeze(0), encoder_output, None, decoder_mask)
+            Proj_output = torch.softmax(model.projectionLayer(decoder_output), dim=-1) #(1,seq,vocab)
+            output = Proj_output[0, -1, :]
 
-        next_token = torch.argmax(output[-1])
-        decoder_input = torch.cat([decoder_input, next_token.unsqueeze(0)])
-        tokens = [tokenizer_tgt.id_to_token(idx.item()) for idx in decoder_input]
-        print(tokens)
+            next_token = torch.argmax(output[-1])
+            decoder_input = torch.cat([decoder_input, next_token.unsqueeze(0)])
+            tokens = [tokenizer_tgt.id_to_token(idx.item()) for idx in decoder_input]
+            print(tokens)
+
+            if next_token == tokenizer_tgt.token_to_id("[EOS]"):
+                break
+
 
 if __name__ == '__main__':
     sentence = "I like to eat alot"
--- LLM/Transformer_Implementation/train.py
+++ LLM/Transformer_Implementation/train.py
@@ -73,6 +73,7 @@ def train_model(config):
     if config['preload']:
         model_filename = get_weights_file_path(config, config['preload'])
         state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
         initial_epoch = state['epoch'] + 1
         optimizer.load_state_dict(state['optimizer_state_dict'])
         global_step = state['global_step']
