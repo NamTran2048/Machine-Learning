Fixed gradient decay, Achieved 12 layers, loss of 1.5 compare to 2.3 previously. Training much more stable.

LLM/Transformer_Implementation/eval.py
@@ -32,28 +32,28 @@ def translate(sentence, config):
     text = torch.tensor(tokenizer_src.encode(sentence).ids)
     print(text)
 
-    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size())
-
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
 
     model.eval()
     with torch.no_grad():
         #Retrieve latest model
         if (config['preload']):
             model_filename = get_weights_file_path(config, config['preload'])
-            state = torch.load(model_filename)
+            state = torch.load(model_filename, map_location=device)
             model.load_state_dict(state['model_state_dict'])
 
         #Encoder and decoder input/output
-        encoder_input = get_encoder_input(text, tokenizer_src, tokenizer_tgt, text.size(-1))
+        encoder_input = get_encoder_input(text, tokenizer_src, tokenizer_tgt, text.size(-1)).to(device)
         encoder_output = model.encode(encoder_input.unsqueeze(0), None) #No masking, (1, seq)
-        decoder_input = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
+        decoder_input = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64).to(device)
     
     
 
         #Inference loop
 
         while True:
-            decoder_mask = get_decoder_mask(len(decoder_input))
+            decoder_mask = get_decoder_mask(len(decoder_input)).to(device)
 
             decoder_output = model.decode(decoder_input.unsqueeze(0), encoder_output, None, decoder_mask)
             Proj_output = torch.softmax(model.projectionLayer(decoder_output), dim=-1) #(1,seq,vocab)
@@ -64,13 +64,13 @@ def translate(sentence, config):
             next_token = torch.argmax(output)
             decoder_input = torch.cat([decoder_input, next_token.unsqueeze(0)])
             tokens = [tokenizer_tgt.id_to_token(idx.item()) for idx in decoder_input]
-            print(tokens)
+            print(" ".join(tokens))
 
             if next_token == tokenizer_tgt.token_to_id("[EOS]"):
                 break
 
 
 if __name__ == '__main__':
-    sentence = "How are you alot"
+    sentence = " A man should be independent ."
     config = get_config()
     translate(sentence, config)

LLM/Transformer_Implementation/model.py
@@ -124,7 +124,7 @@ class residual(nn.Module):
         self.norm = layerNorm()
 
     def forward(self, x, Sublayer):
-        return x + self.dropout((Sublayer(self.norm(x))))
+        return x + self.dropout((Sublayer(self.norm(x))) * (1/(math.sqrt(12))))
 
 class encoderBlock(nn.Module):
 
@@ -132,7 +132,6 @@ class encoderBlock(nn.Module):
         super().__init__()
         self.attention_block = attention_block
         self.FFN_block = FFN_block
-        self.dropout = dropout
         self.residualConnection = nn.ModuleList([residual(dropout), residual(dropout)])
 
     def forward(self, x, mask):
@@ -159,7 +158,6 @@ class decoderBlock(nn.Module):
         self.attention_block = attention_block
         self.cross_block = cross_attention
         self.FFN_block = FFN_block
-        self.dropout = dropout
         self.norm = layerNorm()
         self.residualConnection = nn.ModuleList([residual(dropout) for _ in range(3)])
 
@@ -217,7 +215,7 @@ class Transformer(nn.Module):
     
 
 
-def Model(encoder_vocab_size, decoder_vocab_size, encoder_seq, decoder_seq, d_model, head = 8, N=6, dropout=0.1):
+def Model(encoder_vocab_size, decoder_vocab_size, encoder_seq, decoder_seq, d_model, head = 8, N=12, dropout=0.1):
     #Embedding
     encoder_embedding = inputEmbedding(d_model, encoder_vocab_size)
     decoder_embedding = inputEmbedding(d_model, decoder_vocab_size)

LLM/Transformer_Implementation/train.py
@@ -64,11 +64,11 @@ def train_model(config):
 
     train_dataloader, eval_dataloader, tokenizer_src, tokenizer_tgt = get_data(config)
     model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
-    optimizer = torch.optim.AdamW(model.parameters(), lr= 1.0, eps=1e-9, weight_decay=1e-2)
+    optimizer = torch.optim.AdamW(model.parameters(), lr= 0.5, eps=1e-9, weight_decay=1e-2)
 
     def lr_lambda(step):
         step = max(step, 1)
-        warmup_steps = 4000
+        warmup_steps = 10000
         return (config['d_model'] ** -0.5) * min(step ** -0.5, step * warmup_steps ** -1.5)
 
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
@@ -78,7 +78,7 @@ def train_model(config):
     initial_epoch = 0
     if config['preload']:
         model_filename = get_weights_file_path(config, config['preload'])
-        state = torch.load(model_filename)
+        state = torch.load(model_filename, map_location=device)
         model.load_state_dict(state['model_state_dict'])
         initial_epoch = state['epoch'] + 1
         optimizer.load_state_dict(state['optimizer_state_dict'])